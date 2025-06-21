61279df0bf5f31dddc5a0c55478f689d3f7f6214
Jenkins247 <ealferyev@gmail.com>
2025-06-21 15:55:51 +0500

    init interfaces in repository

---
+++ src/main/java/com/github/jenkins/notifications/notification_service/repository/INotificationRepository.java
@@ -0,0 +1,10 @@
+package com.github.jenkins.notifications.notification_service.repository;
+
+import com.github.jenkins.notifications.notification_service.model.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface INotificationRepository extends JpaRepository<Notification, Long> {
+
+}
