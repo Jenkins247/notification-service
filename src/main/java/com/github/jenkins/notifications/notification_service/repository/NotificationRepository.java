3f7010147355f678e7dc2144697599bc1648fb01
Jenkins247 <ealferyev@gmail.com>
2025-06-22 16:10:41 +0500

    interfaces for repositories

--- src/main/java/com/github/jenkins/notifications/notification_service/repository/NotificationRepository.java
+++ src/main/java/com/github/jenkins/notifications/notification_service/repository/NotificationRepository.java
@@ -1,4 +1,10 @@
 package com.github.jenkins.notifications.notification_service.repository;
 
-public class NotificationRepository {
+import com.github.jenkins.notifications.notification_service.model.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
 }
--- src/main/java/com/github/jenkins/notifications/notification_service/repository/NotificationTemplateRepository.java
+++ src/main/java/com/github/jenkins/notifications/notification_service/repository/NotificationTemplateRepository.java
@@ -1,4 +1,10 @@
 package com.github.jenkins.notifications.notification_service.repository;
 
-public class NotificationTemplateRepository {
+import com.github.jenkins.notifications.notification_service.model.NotificationTemplate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationTemplateRepository extends JpaRepository<NotificationTemplate, Long> {
+    NotificationTemplate findByName(String name);
 }
