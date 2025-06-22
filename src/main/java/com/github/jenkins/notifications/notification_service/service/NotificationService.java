659aec36dc80e72724225b6f84e635557f5be835
Jenkins247 <ealferyev@gmail.com>
2025-06-22 16:10:54 +0500

    interfaces for services

--- src/main/java/com/github/jenkins/notifications/notification_service/service/NotificationService.java
+++ src/main/java/com/github/jenkins/notifications/notification_service/service/NotificationService.java
@@ -1,4 +1,17 @@
 package com.github.jenkins.notifications.notification_service.service;
 
-public class NotificationService {
+import com.github.jenkins.notifications.notification_service.dto.request.NotificationRequestDto;
+import com.github.jenkins.notifications.notification_service.dto.response.NotificationResponseDto;
+import org.springframework.data.domain.Page;
+
+public interface NotificationService {
+    Page<NotificationResponseDto> getAllNotifications(int page, int size, String sortBy, String sortDir);
+
+    NotificationResponseDto getNotificationById(Long id);
+
+    NotificationResponseDto createNotification(NotificationRequestDto request);
+
+    NotificationResponseDto updateNotification(Long id, NotificationRequestDto request);
+
+    void deleteNotification(Long id);
 }
--- src/main/java/com/github/jenkins/notifications/notification_service/service/NotificationTemplateService.java
+++ src/main/java/com/github/jenkins/notifications/notification_service/service/NotificationTemplateService.java
@@ -1,4 +1,20 @@
 package com.github.jenkins.notifications.notification_service.service;
 
-public class NotificationTemplateService {
+import com.github.jenkins.notifications.notification_service.model.NotificationTemplate;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface NotificationTemplateService {
+    NotificationTemplate createTemplate(NotificationTemplate template);
+
+    Optional<NotificationTemplate> getTemplateById(Long id);
+
+    Optional<NotificationTemplate> getTemplateByName(String name);
+
+    List<NotificationTemplate> getAllTemplates();
+
+    NotificationTemplate updateTemplate(Long id, NotificationTemplate template);
+
+    void deleteTemplate(Long id);
 }
